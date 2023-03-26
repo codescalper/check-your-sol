1ffbbd4aa98b0f3e3d6b0043492c34e67e47f2f1
Mayank Singh <msfunbook@gmail.com>
2023-03-26 14:18:09 +0000

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
 import Index from './components';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <>
     <Index />
+    <Footer />
+    </>
   );
 }
 
